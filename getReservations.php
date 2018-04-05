ff15195b2ef9c298a6046be140c2a999038b07a9
Saku Junni <junnisaku@gmail.com>
2018-04-05 13:37:08 +0300

    aaacacacac

--- getReservations.php
+++ getReservations.php
@@ -7,13 +7,13 @@
  */
 
 include 'connect.php';
-$sql = "select * from reservations";
+$sql = "select * from reservation";
 $result = $db->query($sql);
 // Selects and echoes all reservations from database to the index.php page
 if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-        //echo $row["thread_title"] . $row["thread_starter"] . $row["thread_content"];
-        echo"
+        echo $row["ReservationID"] . $row["date"] . $row["time"] . $row["details"];
+        /* echo"
         <div class='row threadContainer'>
           <div class=col-md-12>
             <h4><a  href='viewThread.php?id=$row[thread_id]&title=$row[thread_title]'>$row[thread_title]</a></h4>
@@ -28,6 +28,6 @@ if($result->num_rows > 0){
           <div class=col-md-4>
             <p class='threadId'>#$row[thread_id]</p>
           </div>
-        </div>";
+        </div>"; */
     }
 }
