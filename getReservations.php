ecae69971dbebb2e25b26207978f8c99b970c29b
Saku Junni <junnisaku@gmail.com>
2018-04-05 13:38:50 +0300

    8977

--- getReservations.php
+++ getReservations.php
@@ -12,7 +12,7 @@ $result = $db->query($sql);
 // Selects and echoes all reservations from database to the index.php page
 if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-        echo $row["ReservationID"] . $row["date"] . $row["time"] . $row["details"];
+        echo "Toimii"; // $row["ReservationID"] . $row["date"] . $row["time"] . $row["details"];
         /* echo"
         <div class='row threadContainer'>
           <div class=col-md-12>
