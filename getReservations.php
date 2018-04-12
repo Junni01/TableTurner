9462612e57eb96e7a5ef0185b4cf843f94e91c2e
Saku Junni <junnisaku@gmail.com>
2018-04-12 14:07:54 +0300

    taabeli

--- getReservations.php
+++ getReservations.php
@@ -67,22 +67,10 @@ if($result->num_rows > 0){
             "<td>".
             $row["details"].
             "</td>".
+            "<td>".
+            "<input type='radio'>".
+            "</td>".
             "</tr>";
-        /* echo"
-        <div class='row threadContainer'>
-          <div class=col-md-12>
-            <h4><a  href='viewThread.php?id=$row[thread_id]&title=$row[thread_title]'>$row[thread_title]</a></h4>
-            $row[thread_content]
-            <hr>
-          </div>
-          <div class='col-md-4'>
-            <p class='threadStarter'>$row[thread_starter]</p>
-          </div>
-          <div class='col-md-4'>
-          </div>
-          <div class=col-md-4>
-            <p class='threadId'>#$row[thread_id]</p>
-          </div>
-        </div>"; */
+
     }
 }
