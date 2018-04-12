b82af0ccd932fb8d01b670268d0434934f031a48
Joonas93 <jeskonheimo@gmail.com>
2018-04-12 14:04:40 +0300

    dfd

--- session.php
+++ session.php
@@ -10,14 +10,14 @@ include "connect.php";
 
 if(isset($_SESSION['login_user'])){
     $testii = "hei";
-    $sfName = mysqli_query($db, "SELECT firstName FROM user WHERE email = '$l_email'");
-    $slName = mysqli_query($db, "SELECT lastName FROM user WHERE email = '$l_email'");
-    $_SESSION['fName'] = $sfName;
-    $_SESSION['lName'] = $slName;
     $user_check = $_SESSION['login_user'];
     $ses_sql = mysqli_query($db, "select email FROM user where email = '$user_check' ");
     $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
     $login_session = $row['email'];
+    $sfName = mysqli_query($db, "SELECT firstName FROM user WHERE email ='$user_check' ");
+    $slName = mysqli_query($db, "SELECT lastName FROM user WHERE email = '$user_check'");
+    $_SESSION['fName'] = $sfName;
+    $_SESSION['lName'] = $slName;
 }
 
 
