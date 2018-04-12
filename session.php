eb26f843582fad90d997c02d47bf607ddcdc66e4
Joonas93 <jeskonheimo@gmail.com>
2018-04-12 14:24:16 +0300

    dsddsadd

--- session.php
+++ session.php
@@ -11,17 +11,19 @@ include "connect.php";
 if(isset($_SESSION['login_user'])){
     $testii = "hei";
     $user_check = $_SESSION['login_user'];
-    $ses_sql = mysqli_query($db, "select email FROM user where email = '$user_check' ");
+    $ses_sql = mysqli_query($db, "select email, firstName, lastName, userID FROM user where email = '$user_check' ");
     $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
     $login_session = $row['email'];
-    $sql1 = mysqli_query($db, "SELECT firstName FROM user WHERE email ='$user_check' ");
-    $sql2 = mysqli_query($db, "SELECT lastName FROM user WHERE email = '$user_check'");
-    $row1 = mysqli_fetch_array($sql1, MYSQLI_ASSOC);
-    $row2 = mysqli_fetch_array($sql2, MYSQLI_ASSOC);
-    $slName = $row2['lastName'];
-    $sfName = $row1['firstName'];
+    //$sql1 = mysqli_query($db, "SELECT firstName FROM user WHERE email ='$user_check' ");
+   // $sql2 = mysqli_query($db, "SELECT lastName FROM user WHERE email = '$user_check'");
+    //$row1 = mysqli_fetch_array($sql1, MYSQLI_ASSOC);
+    //$row2 = mysqli_fetch_array($sql2, MYSQLI_ASSOC);
+    $slName = $row['lastName'];
+    $sfName = $row['firstName'];
+    $sUserID = $row['userID'];
     $_SESSION['fName'] = $sfName;
     $_SESSION['lName'] = $slName;
+    $_SESSION['userID'] = $sUserID;
 }
 
 
