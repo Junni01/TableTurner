3e72ff383fe3c6c7c3ac85d3f7fb45cb3dc54d86
Saku Junni <junnisaku@gmail.com>
2018-04-19 14:16:00 +0300

    uliuli

--- getTimes.php
+++ getTimes.php
@@ -14,7 +14,7 @@ $time = array(0,0,0,0,0,0,0,0,0,0);
 $timeconversion = array('15:00', '16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00','24:00');
 
             echo "<h1>Vapaat ajat</h1>
-                   <p>Tässä vapaana olevat ajat valitsemallenne päivälle</p>
+                   <p>Tässä vapaana olevat ajat valitsemallenne päivälle: ". $date ."</p>
             <table class='table table-bordered'>
                 <thead>
                     <tr>
